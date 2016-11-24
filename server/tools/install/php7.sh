39c28830da84f296fca42dcc617692674feec897
CrowCrowzard <crowizard@gmail.com>
2016-11-24 12:42:14 +0900

    install php7 issue

--- server/tools/install/php7.sh
+++ server/tools/install/php7.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+#TODO can't install php7 instead of installing php5
 yum -y remove php-*
 yum -y install --enablerepo=remi,remi-php70 php php-devel php-opcache php-mbstring php-mcrypt php-pdo php-gd php-mysqlnd php-pecl-xdebug php-fpm php-xml
