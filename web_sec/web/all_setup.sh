a7690c0d8ee1f1f971b47ccffe5ef4a73ce52580
CrowCrowzard <crowizard@gmail.com>
2016-11-28 18:08:35 +0900

    Don't use mv and use cp to /var/www/html

--- web_sec/web/all_setup.sh
+++ web_sec/web/all_setup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-# TODO PHP5 のままだった
-# install php7 httpd 
+# install php7 httpd
 tar zxvf apache_install.tar.gz
 ./apache_install.sh
 rm apache_install.sh apache_install.tar.gz
 
+# TODO php-pdoインストール消す
 # install mysql
 tar zxvf mysql_install.tar.gz
 ./mysql_install.sh
@@ -15,13 +15,13 @@ rm mysql_install.tar.sh mysql_install.tar.gz
 # sql_injection site
 tar zxvf sql.tar.gz
 ./sql_db.sh
-mv sql.php /var/www/html
-rm sql_db.sh sql_db.sql sql.tar.gz first_user.sql users.sql
+cp sql.php /var/www/html # mvするとファイルディスクリプタがおかしくなる
+rm sql.phpsql_db.sh sql_db.sql sql.tar.gz first_user.sql users.sql
 
 # xss_reflected site
 tar zxvf xss_reflected.tar.gz
-mv xss_reflected.php /var/www/html
-rm xss_reflected.tar.gz
+cp xss_reflected.php /var/www/html
+rm xss_reflected.php xss_reflected.tar.gz
 
 # TODO login function
 # TODO chmod +x stored_db_reset.sh
@@ -29,20 +29,19 @@ rm xss_reflected.tar.gz
 # xss_stored site
 tar zxvf xss_stored.tar.gz
 ./stored_db.sh
-mv xss_stored.php /var/www/html
-rm stored_db.sh stored_db.sql xss_stored.tar.gz first_comment.sql guestbook.sql
+cp xss_stored.php /var/www/html
+rm stored_db.sh stored_db.sql xss_stored.php xss_stored.tar.gz first_comment.sql guestbook.sql
 
 # TODO chmod +x create
 # TODO shellscript mv destination
 # directory_traversal site
 tar zxvf directory_traversal.tar.gz
-mv readfile.php /var/www/html
-rm create_document_directory.sh directory_traversal.tar.gz
+cp readfile.php /var/www/html
+rm create_document_directory.sh directory_traversal.tar.gz readfile.php
 
 # TODO typo tar file
 # TODO change search site not mail site
 # command_execution site
 tar zxvf command_exectution.tar.gz
-mv send_mail.php /var/www/html
-rm command_execution.tar.gz
-
+cp send_mail.php /var/www/html
+rm command_execution.tar.gz send_mail.php
