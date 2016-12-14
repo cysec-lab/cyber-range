37d4dfd0a1028f29f3f06ba400911345e23592a7
CrowCrowzard <crowizard@gmail.com>
2016-12-14 13:31:46 +0900

    selinux off

--- web_sec/web/all_setup.sh
+++ web_sec/web/all_setup.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
 
+#TODO
+# SELinux off
+FILENAME='/etc/selinux/config'
+sed -i -e "s/SELINUX=enforcing/SELINUX=disabled" $FILENAME
+
+#TODO not reboot apache_install.sh
+# それ以外OK
 # install php7 httpd
 tar zxvf apache_install.tar.gz
 ./apache_install.sh
 rm apache_install.sh apache_install.tar.gz
 
+#TODO 自動化するとなぜかインストールしない
 # install mysql
 tar zxvf mysql_install.tar.gz
 ./mysql_install.sh
-rm mysql_install.tar.sh mysql_install.tar.gz
+rm mysql_install.sh mysql_install.tar.gz
 
 # sql_injection site
 tar zxvf sql.tar.gz
@@ -17,6 +25,7 @@ cp sql.php /var/www/html # mvするとファイルディスクリプタがおか
 rm sql.phpsql_db.sh sql_db.sql sql.tar.gz first_user.sql users.sql
 
 # xss_reflected site
+# OK
 tar zxvf xss_reflected.tar.gz
 cp xss_reflected.php /var/www/html
 rm xss_reflected.php xss_reflected.tar.gz
@@ -29,13 +38,17 @@ cp login.php logout.php xss_stored.php /var/www/html
 rm login.php logout.php stored_db.sh stored_db.sql xss_stored.php xss_stored.tar.gz first_comment.sql guestbook.sql
 
 # directory_traversal site
+# ok selinux
 tar zxvf directory_traversal.tar.gz
+./create_document_directory.sh
 cp readfile.php /var/www/html
 rm create_document_directory.sh directory_traversal.tar.gz readfile.php
 
-# TODO typo tar file
-# TODO change search site not mail site
 # command_execution site
+# ok send_mail.php change
 tar zxvf command_execution.tar.gz
 cp send_mail.php /var/www/html
 rm command_execution.tar.gz send_mail.php
+
+# refere selinux
+reboot
