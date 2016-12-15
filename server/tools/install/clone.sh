ba9a629956457a3bfa8db59eb2832557cd92d756
CrowCrowzard <crowizard@gmail.com>
2016-12-15 16:32:36 +0900

    DNS GATEWAY BROADCAST setup

--- server/tools/install/clone.sh
+++ server/tools/install/clone.sh
@@ -28,6 +28,9 @@ attr=${conf#*ATTR{address\}==\"}
 sed -i -e "s/HWADDR=.*$/HWADDR=${attr%%\"*}/g" $ETHFILE
 
 sed -i -e "s/IPADDR=.*$/IPADDR=$1/g" $ETHFILE
+sed -i -e "s/DNS1=.*$/DNS1=${1%.*}.1/g" $ETHFILE
+sed -i -e "s/GATEWAY=.*$/GATEWAY=${1%.*}.1/g" $ETHFILE
+sed -i -e "s/BROADCAST=.*$/BROADCAST=${1%.*}.255/g" $ETHFILE
 
 ./chg_hostname.sh $2
 
