a4f889096ad394e8389222efa8834b7710eb4a51
CrowCrowzard <crowizard@gmail.com>
2016-12-21 14:06:46 +0900

    change edit file

--- server/tools/install/serial_console.sh
+++ server/tools/install/serial_console.sh
@@ -1,10 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
-FILENAME='/etc/grub.conf'
-sed -i -e "s/quiet rhgb/console=ttyS0,115200/g" $FILENAME
+FILENAME='/boot/grub/grub.conf'
+sed -i -e "s/quiet/quiet console=ttyS0,115200/g" $FILENAME
 
-ADDSENTENCE="serial --speed=115200 --unit=0 --word=8 --parity=no --stop=1\n"
-ADDSENTENCE=$ADDSENTENCE"terminal --timeout=5 serial console\n"
+ADDSENTENCE="serial --speed=115200 --unit=0 --word=8 --parity=no --stop=1\n" 
+ADDSENTENCE=$ADDSENTENCE"terminal --timeout=5 serial console\n" 
 
 # 改行文字の問題解決
 echo ${ADDSENTENCE%\\n} | sed -e 's/\\n/\n/g' >> $FILENAME
+
+echo 'ttyS0' >> '/etc/securetty'
