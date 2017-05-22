037dfea09fb2f0fab52eed0a009586a0012a2c2a
Yuta Muraki <crowizard@gmail.com>
2017-05-22 10:34:21 +0900

    fix error

--- server/tools/proxmox/create.sh
+++ server/tools/proxmox/create.sh
@@ -16,7 +16,7 @@ SOCKETS=1
 CORES=1
 
 
-pvesh create /nodes/proxmox/storage/local/content -filename vm-${ID}-disk-1.{$FORMAT} -format ${FORMAT} -size {$SIZE}G -vmid ${ID}
-pvesh create /nodes/proxmox/qemu -vmid {$ID} -memory {$MEMORY} -sockets {$SOCKETS} -cores {$CORES} -net0 e1000,bridge=vmbr0 -ide0 local:{$ID}/vm-{$ID}-disk-1.{$FORMAT} -ide2 local:iso/CentOS-6.7-x86_64-minimal.iso,media=cdrom
+pvesh create /nodes/proxmox/storage/local/content -filename vm-${ID}-disk-1.${FORMAT} -format ${FORMAT} -size ${SIZE}G -vmid ${ID}
+pvesh create /nodes/proxmox/qemu -vmid ${ID} -memory ${MEMORY} -sockets ${SOCKETS} -cores ${CORES} -net0 e1000,bridge=vmbr0 -ide0 local:${ID}/vm-${ID}-disk-1.${FORMAT} -ide2 local:iso/CentOS-6.7-x86_64-minimal.iso,media=cdrom
 pvesh create /nodes/proxmox/qemu/${ID}/status/start
 
