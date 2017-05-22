cb46dfadd187c10c6a68c30ca08dce1501a0e150
Yuta Muraki <crowizard@gmail.com>
2017-05-22 10:25:15 +0900

    VM作成を柔軟にする

--- server/tools/proxmox/create.sh
+++ server/tools/proxmox/create.sh
@@ -1,4 +1,22 @@
-pvesh create /nodes/proxmox/storage/local/content -filename vm-106-disk-1.qcow2 -format qcow2 -size 64G -vmid 106
-pvesh create /nodes/proxmox/qemu -vmid 106 -memory 1024 -sockets 1 -cores 1 -net0 e1000,bridge=vmbr0 -ide0 local:106/vm-106-disk-1.qcow2 -ide2 local:iso/CentOS-6.7-x86_64-minimal.iso,media=cdrom
-pvesh create /nodes/proxmox/qemu/106/status/start
+#!/bin/bash
+
+# Need create vmid
+if [ $# -ne 1 ]; then
+  echo "Need [vmid]"
+  echo "$0 [100]"
+  exit
+fi
+
+ID=$1
+# TODO choose resource info
+FORMAT='qcow2'
+SIZE=64
+MEMORY=1024
+SOCKETS=1
+CORES=1
+
+
+pvesh create /nodes/proxmox/storage/local/content -filename vm-${ID}-disk-1.{$FORMAT} -format ${FORMAT} -size {$SIZE}G -vmid ${ID}
+pvesh create /nodes/proxmox/qemu -vmid {$ID} -memory {$MEMORY} -sockets {$SOCKETS} -cores {$CORES} -net0 e1000,bridge=vmbr0 -ide0 local:{$ID}/vm-{$ID}-disk-1.{$FORMAT} -ide2 local:iso/CentOS-6.7-x86_64-minimal.iso,media=cdrom
+pvesh create /nodes/proxmox/qemu/${ID}/status/start
 
