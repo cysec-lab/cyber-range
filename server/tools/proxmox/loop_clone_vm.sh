80d60a210a4ae40dc8af97d3d61a7969c31b2fcc
root <root@pve.local>
2017-11-28 17:26:55 +0900

    vm cloneの修正

--- server/tools/proxmox/loop_clone_vm.sh
+++ server/tools/proxmox/loop_clone_vm.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #TODO VM IPアドレス
 
-if [ $# -ne 3 ]; then
-    echo "[PC TYPE] [TEMPLATE_NUM] [VM NUM]  need"
+if [ $# -ne 4 ]; then
+    echo "[PC TYPE] [TEMPLATE_NUM] [VM NUM] [Loop Count] need"
     echo "example:"
-    echo "./clone_vm.sh web 719 111"
+    echo "$0 web 719 111 5"
     exit 1
 fi
 
@@ -15,12 +15,18 @@ fi
 PC_TYPE=$1
 TEMPLATE_NUM=$2
 CLONE_NUM=$3
+LOOP_COUNT=$4
 VM_NAME=$PC_TYPE$CLONE_NUM
 #IP_ADDRESS="192.168.1${CLONE_NUM:1:1}0.${CLONE_NUM:1:2}"
 #TEMPLATE_NAME=$PC_TYPE$TEMPLATE_NUM
 
 # clone
-qm clone $TEMPLATE_NUM $CLONE_NUM --name $VM_NAME --full #--format raw --full
+for i in `seq 1 $LOOP_COUNT`
+do
+    clone_num=`expr $CLONE_NUM + $i - 1`
+    ./clone_vm.sh $PC_TYPE  $TEMPLATE_NUM $clone_num
+done
+#qm clone $TEMPLATE_NUM $CLONE_NUM --name $VM_NAME --full #--format raw --full
 
 #./disk_mount.sh $CLONE_NUM $IP_ADDRESS $PC_TYPE $TEMPLATE_NAME
 
