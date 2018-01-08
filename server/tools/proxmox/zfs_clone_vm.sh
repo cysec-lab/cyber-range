def8bf306fdca345b3545a711820b47608158c9c
hedgehoCrow <crowizard@gmail.com>
2018-01-08 11:08:52 +0900

    add script timer

--- server/tools/proxmox/zfs_clone_vm.sh
+++ server/tools/proxmox/zfs_clone_vm.sh
@@ -14,8 +14,6 @@ CLONE_NUM=$3
 VM_NAME=$PC_TYPE$CLONE_NUM
 TEMPLATE_CONFIG_PATH=/etc/pve/qemu-server/${TEMPLATE_NUM}.conf
 CLONE_CONFIG_PATH=/etc/pve/qemu-server/${CLONE_NUM}.conf
-#IP_ADDRESS="192.168.1${CLONE_NUM:1:1}0.${CLONE_NUM:1:2}"
-#TEMPLATE_NAME=$PC_TYPE$TEMPLATE_NUM
 
 # create snapshot
 zfs snapshot rpool/data/vm-${TEMPLATE_NUM}-disk-1@${TEMPLATE_NUM}_snapshot
@@ -27,13 +25,15 @@ zfs clone rpool/data/vm-${TEMPLATE_NUM}-disk-1@${TEMPLATE_NUM}_snapshot rpool/da
 cp $TEMPLATE_CONFIG_PATH $CLONE_CONFIG_PATH
 sed -i -e "s/vm-${TEMPLATE_NUM}-disk-1/vm-${CLONE_NUM}-disk-1/g" $CLONE_CONFIG_PATH
 sed -i -e "s/^name:.*/name: ${VM_NAME}/g" $CLONE_CONFIG_PATH
+
+# TODO
+# change vm config files
+
+# start vm
 qm start $CLONE_NUM
 
 
 #./disk_mount.sh $CLONE_NUM $IP_ADDRESS $PC_TYPE $TEMPLATE_NAME
-
-# start vm
-#qm start $CLONE_NUM
 
 
 # serial console connection setup
