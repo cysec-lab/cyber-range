c6b3dd97dd9a52b34669b1a216e9f303c53ea1e5
hedgehoCrow <crowizard@gmail.com>
2018-03-10 11:52:23 +0900

    python3 install update

--- server/tools/install/python3.sh
+++ server/tools/install/python3.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
-yum -y install yum-utils
-yum-builddep python
+yum -y install https://centos6.iuscommunity.org/ius-release.rpm
+yum install -y python35*
 
-cd /opt
-curl -O https://www.python.org/ftp/python/3.5.1/Python-3.5.1.tgz
-tar zxvf Python-3.5.1.tgz
-cd Python-3.5.1
-./configure
-make
-make install
+ln -s /usr/bin/python3.5 /usr/bin/python3
+ln -s /usr/bin/pip3.5 /usr/bin/pip3
+
+#yum -y install yum-utils
+#yum-builddep python
+#
+#cd /opt
+#curl -O https://www.python.org/ftp/python/3.5.1/Python-3.5.1.tgz
+#tar zxvf Python-3.5.1.tgz
+#cd Python-3.5.1
+#./configure
+#make
+#make install
 
