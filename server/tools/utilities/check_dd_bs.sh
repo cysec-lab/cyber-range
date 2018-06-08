79bea2e7492382c2a13d68f08314b5654acb5797
Yuta Muraki <crowizard@gmail.com>
2018-06-08 14:03:39 +0900

    bs確認の値に1,2,4Gを追加

--- server/tools/utilities/check_dd_bs.sh
+++ server/tools/utilities/check_dd_bs.sh
@@ -5,7 +5,7 @@
 echo "creating a file to work with"
 dd if=/dev/zero of=/var/tmp/infile count=1175000
 
-for bs in  16M 32M 64M 128M 256M 512M
+for bs in  16M 32M 64M 128M 256M 512M 1G 2G 4G
 
 do
         echo "---------------------------------------"
