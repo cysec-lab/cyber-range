125dab499d6cee1641966b4b0a7eb2b2a89ac695
hedgehoCrow <crowizard@gmail.com>
2018-08-04 16:50:37 +0900

    fix send snapshot script

--- server/tools/proxmox/send_snapshot.sh
+++ server/tools/proxmox/send_snapshot.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
-if [ $# -ne 4 ]; then
-    echo "[vm num] [Pool Name] [VM SIZE] [qcow2 file full path] need"
+if [ $# -ne 3 ]; then
+    echo "[vm num] [Src Pool Name] [Dist Pool Name] need"
     echo "example:"
-    echo "$0 100 rpool 32 /var/lib/vz/images/900/vm-900-disk-1.qcow2"
+    echo "$0 100 tvmpool rpool"
     exit 1
 fi
 
 VM_NUM=$1
 SRC_POOL_NAME=$2
 DIST_POOL_NAME=$3
-SRC_ZFS_FILE_PATH=$SRC_POOL_NAME/data/vm-${VM_NUM}-disk-1
+SRC_ZFS_SNAPSHOT=$SRC_POOL_NAME/data/vm-${VM_NUM}-disk-1@${VM_NUM}_snapshot
 DIST_ZFS_FILE_PATH=$DIST_POOL_NAME/data/vm-${VM_NUM}-disk-1
+DIST_ZFS_SNAPSHOT=${DIST_ZFS_FILE_PATH}@${VM_NUM}_snapshot
 
-# スナップショットがあるか判定
-zfs snapshot -r tvmpool/data/vm-902-disk-1@902_snapshot
+# スナップショットがあるか判定＋ない場合は作成
+result=`zfs list -t snapshot | grep ${SRC_ZFS_SNAPSHOT}`
+if [ ${#result} -eq 0 ]; then
+	zfs snapshot -r $SRC_ZFS_SNAPSHOT
+fi
 
-# プールが存在しているのかの判定＋削除
-time zfs send tvmpool/data/vm-902-disk-1@902_snapshot | zfs receive -F rpool/data/vm-902-disk-1
+# スナップショットがあるか判定＋ある場合は削除
+result=`zfs list -t snapshot | grep ${DIST_ZFS_SNAPSHOT}`
+if [ ${#result} -eq 0 ]; then
+	zfs destroy $DIST_ZFS_SNAPSHOT
+fi
+
+# スナップショットの送信
+time zfs send $SRC_ZFS_SNAPSHOT | zfs receive -F $DIST_ZFS_FILE_PATH
