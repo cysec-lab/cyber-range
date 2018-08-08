e10294d72d6330c430fe2c16f1c1806d18d8fbcb
hedgehoCrow <crowizard@gmail.com>
2018-08-08 21:57:57 +0900

    ZFSクローン時にmacアドレスを変更する

--- server/tools/proxmox/clone_vyos.sh
+++ server/tools/proxmox/clone_vyos.sh
@@ -13,6 +13,19 @@ fi
 VM_NUM=$1
 VYOS_NETWORK_BRIDGE=$2
 GROUP_NETWORK_BRIDGE=$3
+
+VM_CONF_FILE="/etc/pve/qemu-server/${VM_NUM}.conf"
+
+VYOS_NETRORK_MAC_ADDRESS=`cat $VM_CONF_FILE | grep net0`
+# MACアドレスの前後の部分を削除
+VYOS_NETRORK_MAC_ADDRESS=${VYOS_NETRORK_MAC_ADDRESS%,*}
+VYOS_NETRORK_MAC_ADDRESS=${VYOS_NETRORK_MAC_ADDRESS#*=}
+
+GROUP_NETRORK_MAC_ADDRESS=`cat $VM_CONF_FILE | grep net1`
+# MACアドレスの前後の部分を削除
+GROUP_NETRORK_MAC_ADDRESS=${GROUP_NETRORK_MAC_ADDRESS%,*}
+GROUP_NETRORK_MAC_ADDRESS=${GROUP_NETRORK_MAC_ADDRESS#*=}
+
 # VYOS_NETWORK_BRIDGEとnetworkアドレスは同じ
 Proxmox_side_IP_ADDRESS="192.168.${VYOS_NETWORK_BRIDGE}.${GROUP_NETWORK_BRIDGE}"
 VyOS_side_NETWORK="192.168.${GROUP_NETWORK_BRIDGE}"
@@ -23,3 +36,7 @@ CONFIG_FILE="$MOUNT_DIR/boot/1.1.7/live-rw/config/config.boot"
 sed -i -e "s/192.168.100.221/$Proxmox_side_IP_ADDRESS/g" $CONFIG_FILE
 sed -i -e "s/192.168.110/$VyOS_side_NETWORK/g" $CONFIG_FILE
 sed -i -e "s/192.168.100/${Proxmox_side_IP_ADDRESS%.*}/g" $CONFIG_FILE
+
+sed -i -e "0,/hw-id/s/hw-id.*/hw-id ${VYOS_NETWORK_BRIDGE}/g" $CONFIG_FILE
+#sed -i -e "0,/hw-id/!{0,/hw-id/s/hw-id.*/hw-id ${GROUP_NETRORK_MAC_ADDRESS}/}" $CONFIG_FILE
+awk '/hw-id/{c++;if(c==2){sub("hw-id.*","hw-id $GROUP_NETRORK_MAC_ADDRESS");c==0}}1' $CONFIG_FILE >& /dev/null
--- server/tools/proxmox/zfs_clone_vm.sh
+++ server/tools/proxmox/zfs_clone_vm.sh
@@ -16,6 +16,7 @@ TEMPLATE_CONFIG_PATH=/etc/pve/qemu-server/${TEMPLATE_NUM}.conf
 CLONE_CONFIG_PATH=/etc/pve/qemu-server/${CLONE_NUM}.conf
 SNAPSHOT=rpool/data/vm-${TEMPLATE_NUM}-disk-1@${TEMPLATE_NUM}_snapshot
 
+
 # check snapshot
 snapshot_check_cmd="zfs list -r -t snapshot -o name,creation rpool"
 eval "$snapshot_check_cmd | grep $SNAPSHOT > /dev/null"
@@ -34,9 +35,10 @@ sed -i -e "s/vm-${TEMPLATE_NUM}-disk-1/vm-${CLONE_NUM}-disk-1/g" $CLONE_CONFIG_P
 sed -i -e "s/local-tvm/local-zfs/g" $CLONE_CONFIG_PATH
 sed -i -e "s/^name:.*/name: ${VM_NAME}/g" $CLONE_CONFIG_PATH
 for ((i=0;i<${#BRIDGE_NUMS[@]};i++)); do
-    # bridgeを変更する行のbridge名を変更した新しいルールを作成する
-    # 正規表現でbridgeの前のe1000の情報を保持できず二段階で処理をしている
-    new_rule=`grep "net${i}:" $CLONE_CONFIG_PATH | head -n 1 | sed -e "s/bridge=.*/bridge=vmbr${BRIDGE_NUMS[i]}/g"`
+    # change bridge name
+    # ZFSクローンではmac addressが変更されないので、新たなmac addressを作成し置き換える
+    mac_address=02:`dd if=/dev/urandom bs=1024 count=1 2>/dev/null|md5sum|sed 's/^\(..\)\(..\)\(..\)\(..\)\(..\).*$/\1:\2:\3:\4:\5/'`
+    new_rule="net${i}: e1000=${mac_address},bridge=vmbr${BRIDGE_NUMS[i]}"
     # clone時にconfファイルにparentとして以前のデータが残ることがあるが前のデータを残すために最初にマッチした行のみ変更を加える
-    sed -i -e "0,/^net${i}/s/^net${i}.*bridge=.*/$new_rule/g" $CLONE_CONFIG_PATH
+    sed -i -e "0,/^net${i}/s/^net${i}.*/$new_rule/g" $CLONE_CONFIG_PATH
 done
