4102ad582e038873002de23481eff454ef049bb2
Yuta Muraki <crowizard@gmail.com>
2019-01-15 14:47:31 +0900

    ラベルとタイトルの追加+XY軸を0スタートにする

--- server/tools/utilities/dummy_data_log/create_dummy_data_graph.py
+++ server/tools/utilities/dummy_data_log/create_dummy_data_graph.py
@@ -8,6 +8,9 @@ import csv
 import pandas as pd
 import matplotlib.pyplot as plt
 
+x_label = 'ファイル数'
+y_label = '時間'
+
 argvs = sys.argv
 if (len(argvs) != 2):
     print('Usage: # python3 %s [FILE PATH]' % argvs[0])
@@ -21,8 +24,16 @@ if not os.path.isfile(input_file):
 
 # ファイル読み込み
 df = pd.read_csv(input_file, encoding="utf-8", names=['files', 'time'])
-plt.plot(df['files'], df['time'], marker="o")
 
+# プロットの設定
+plt.plot(df['files'], df['time'])
+plt.title('ZFSクローンとFULLクローンのモデル化')
+plt.xlabel(x_label)
+plt.ylabel(y_label)
+plt.xlim(left=0)
+plt.ylim(bottom=0)
+
+# ファイル出力と表示
 output_file = input_file.replace('csv', 'png')
 plt.savefig(output_file)
 plt.show()
