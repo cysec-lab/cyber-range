9127134b2a5c211fd0ab87247f2711da264efde8
Yuta Muraki <crowizard@gmail.com>
2019-01-15 17:22:03 +0900

    図作成スクリプトを2つのグラフに対応

--- server/tools/utilities/dummy_data_log/create_dummy_data_graph.py
+++ server/tools/utilities/dummy_data_log/create_dummy_data_graph.py
@@ -8,26 +8,27 @@ import csv
 import pandas as pd
 import matplotlib.pyplot as plt
 
-x_label = 'ファイル数'
-y_label = '時間'
+x_label = 'ファイル数[個]'
+y_label = '時間[秒]'
 
 argvs = sys.argv
-if (len(argvs) != 2):
-    print('Usage: # python3 %s [FILE PATH]' % argvs[0])
-    print('Example: # python3 %s ./measure_dummy_data.csv' % argvs[0])
+if (len(argvs) != 3):
+    print('Usage: # python3 %s [ZFS FILE PATH] [FULL FILE PATH]' % argvs[0])
+    print('Example: # python3 %s ./measure_dummy_zfs_data.csv ./measure_dummy_full_data.csv' % argvs[0])
     quit()
 
-input_file = argvs[1]
-if not os.path.isfile(input_file):
-    print(input_file + "is not exist")
+zfs_file  = argvs[1]
+full_file = argvs[2]
+if not os.path.isfile(zfs_file) or not os.path.isfile(full_file):
+    print(zfs_file + " or "+ full_file + " are not exist")
     quit()
 
 # ファイル読み込み
-#df_zfs = pd.read_csv(input_file, encoding="utf-8", names=['files', 'time'])
-df_full = pd.read_csv(input_file, encoding="utf-8", names=['files', 'time'])
+df_zfs  = pd.read_csv(zfs_file, encoding="utf-8", names=['files', 'time'])
+df_full = pd.read_csv(full_file, encoding="utf-8", names=['files', 'time'])
 
 # データをプロット
-#plt.plot(df_zfs['files'], df_zfs['time'], label="zfs")
+plt.plot(df_zfs['files'], df_zfs['time'], label="zfs")
 plt.plot(df_full['files'], df_full['time'], label="full") # TODO: クローン時間差ゲタを履かす
 
 # プロットの設定
@@ -39,7 +40,9 @@ plt.xlim(left=0)
 plt.ylim(bottom=0)
 
 # ファイル出力と表示
-output_file = input_file.replace('csv', 'png') # TODO: ファイル名を考える
-plt.savefig(output_file)
+output_dir, output_file  = os.path.split(zfs_file)
+output_dir = output_dir.replace('zfs', '')
+output_file = output_file.replace('zfs_', '').replace('csv', 'png')
+plt.savefig(output_dir + output_file)
 plt.show()
 
