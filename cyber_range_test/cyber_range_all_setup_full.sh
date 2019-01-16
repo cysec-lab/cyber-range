374d9d3d300cd855bce54182e230efd15d3909a4
hedgehoCrow <crowizard@gmail.com>
2019-01-16 11:42:05 +0900

    fullクローンのターゲットにlocal zfs-local 両方に対応

--- cyber_range_test/cyber_range_all_setup_full.sh
+++ cyber_range_test/cyber_range_all_setup_full.sh
@@ -9,7 +9,7 @@ CLIENT_TEMP_NUM=0 # initial client pc template vm number. RANGE: 100~999
 VYOS_TEMP_NUM=0   # initial vyos(software router os) template vm number. RANGE: 100~999
 
 CLONE_TYPE='full'         # clone type
-TARGET_STRAGE='local-zfs' # full clone target strage
+TARGET_STRAGE='local' # full clone target strage
 VG_NAME='VolGroup'        # Volume Group name
 LOG_FILE="./setup.log"    # log file name
 
@@ -22,7 +22,8 @@ student_per_group=`echo $json_scenario_data | jq '.student_per_group'`
 # bridge number of connecting each group network(=Proxmox number)
 VYOS_NETWORK_BRIDGE=$PROXMOX_NUM
 
-read -p "scenario number(1 or 2): " scenario_num
+#read -p "scenario number(1 or 2): " scenario_num
+scenario_num=1
 if [ $scenario_num -eq 1 ] || [ $scenario_num -eq 2 ]; then
     scenario_data=`echo $json_vm_data | jq ".${CLONE_TYPE}.scenario_nums[$((scenario_num - 1))]"`
     VYOS_TEMP_NUM=`echo $scenario_data | jq '.VYOS_TEMP_NUM'`
@@ -50,8 +51,11 @@ for num in ${VYOS_NUMS[@]}; do
     # bridge rules https://sites.google.com/a/cysec.cs.ritsumei.ac.jp/local/shareddevices/proxmox/network
     group_network_bridge="1${PROXMOX_NUM}${num:0:1}"
     $tool_dir/clone_vm.sh $num $VYOS_TEMP_NUM $pc_type $TARGET_STRAGE $VYOS_NETWORK_BRIDGE $group_network_bridge
-    $tool_dir/zfs_vyos_config_setup.sh $num $VYOS_NETWORK_BRIDGE $group_network_bridge            # change cloned vm's config files
-    # $tool_dir/vyos_config_setup.sh $num $VYOS_NETWORK_BRIDGE $group_network_bridge
+    if [ "$TARGET_STRAGE" = 'zfs-local' ]; then
+        $tool_dir/zfs_vyos_config_setup.sh $num $VYOS_NETWORK_BRIDGE $group_network_bridge            # change cloned vm's config files
+    else
+        $tool_dir/vyos_config_setup.sh $num $VYOS_NETWORK_BRIDGE $group_network_bridge
+    fi
     qm start $num &
 done
 
@@ -61,12 +65,15 @@ for num in ${WEB_NUMS[@]}; do
     group_network_bridge="1${PROXMOX_NUM}${num:0:1}"
     ip_address="192.168.${group_network_bridge}.${num:2:1}"
     $tool_dir/clone_vm.sh $num $WEB_TEMP_NUM $pc_type $TARGET_STRAGE $group_network_bridge
-    $tool_dir/zfs_centos_config_setup.sh $num $ip_address $pc_type $VG_NAME # change cloned vm's config files
-    # $tool_dir/disk_mount.sh $num $ip_address $pc_type $VG_NAME
-    # $tool_dir/uuid_setup.sh $num $ip_address $pc_type $VG_NAME
-    # $tool_dir/centos_config_setup.sh $num $ip_address $pc_type $VG_NAME
-    # $tool_dir/nfs_setup.sh $num $ip_address $pc_type
-    # $tool_dir/disk_umount.sh $num $ip_address $pc_type $VG_NAME
+    if [ "$TARGET_STRAGE" = 'zfs-local' ]; then
+        $tool_dir/zfs_centos_config_setup.sh $num $ip_address $pc_type $VG_NAME # change cloned vm's config files
+    else
+        $tool_dir/disk_mount.sh $num $ip_address $pc_type $VG_NAME
+        $tool_dir/uuid_setup.sh $num $ip_address $pc_type $VG_NAME
+        $tool_dir/centos_config_setup.sh $num $ip_address $pc_type $VG_NAME
+        $tool_dir/nfs_setup.sh $num $ip_address $pc_type
+        $tool_dir/disk_umount.sh $num $ip_address $pc_type $VG_NAME
+    fi
     qm start $num &
 done
 
@@ -76,22 +83,26 @@ for num in ${CLIENT_NUMS[@]}; do
     group_network_bridge="1${PROXMOX_NUM}${num:0:1}"
     ip_address="192.168.${group_network_bridge}.${num:2:1}"
     if [ $scenario_num -eq 2 ]; then
-	mul_num=${num:0:1}
-	mul_num=$((mul_num - 1))
-	add_num=${num:2:1}
-	add_num=$((add_num - 3))
-	client_num=$((CLIENT_TEMP_NUM + student_per_group * mul_num + add_num))
+	#mul_num=${num:0:1}
+	#mul_num=$((mul_num - 1))
+	#add_num=${num:2:1}
+	#add_num=$((add_num - 3))
+	#client_num=$((CLIENT_TEMP_NUM + student_per_group * mul_num + add_num))
+        client_num=$CLIENT_TEMP_NUM
     	$tool_dir/clone_vm.sh $num $client_num $pc_type $TARGET_STRAGE $group_network_bridge
     else
         $tool_dir/clone_vm.sh $num $CLIENT_TEMP_NUM $pc_type $TARGET_STRAGE $group_network_bridge
     fi
     if [ $scenario_num -eq 1 ]; then
-        $tool_dir/zfs_centos_config_setup.sh $num $ip_address $pc_type $VG_NAME # change cloned vm's config files
-        # $tool_dir/disk_mount.sh $num $ip_address $pc_type $VG_NAME
-        # $tool_dir/uuid_setup.sh $num $ip_address $pc_type $VG_NAME
-        # $tool_dir/centos_config_setup.sh $num $ip_address $pc_type $VG_NAME
-        # $tool_dir/nfs_setup.sh $num $ip_address $pc_type
-        # $tool_dir/disk_umount.sh $num $ip_address $pc_type $VG_NAME
+        if [ "$TARGET_STRAGE" = 'zfs-local' ]; then
+            $tool_dir/zfs_centos_config_setup.sh $num $ip_address $pc_type $VG_NAME # change cloned vm's config files
+        else
+            $tool_dir/disk_mount.sh $num $ip_address $pc_type $VG_NAME
+            $tool_dir/uuid_setup.sh $num $ip_address $pc_type $VG_NAME
+            $tool_dir/centos_config_setup.sh $num $ip_address $pc_type $VG_NAME
+            $tool_dir/nfs_setup.sh $num $ip_address $pc_type
+            $tool_dir/disk_umount.sh $num $ip_address $pc_type $VG_NAME
+        fi
     fi
     qm start $num &
 done
