700b30c5d558bffd34706b16c10b66b5cc193335
Yuta Muraki <crowizard@gmail.com>
2019-02-09 16:28:08 +0900

    不要な行の削除

--- server/tools/install/gui.sh
+++ server/tools/install/gui.sh
@@ -2,18 +2,3 @@
 
 FILENAME='/etc/inittab'
 sed -i -e 's/id:3:initdefault:/id:5:initdefault:/g' $FILENAME
-
-HOSTNAME=`hostname`
-
-useradd ${HOSTNAME}
-
-yum -y install expect
-expect -c "
-set timeout 5
-spawn passwd ${HOSTNAME}
-expect \"New password:\"
-send \"${HOSTNAME}\n\"
-expect \"Retype new password:\"
-send \"${HOSTNAME}\n\"
-interact
-"
