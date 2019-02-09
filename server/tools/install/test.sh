6649562ab7c3862fac7148aba8544642ddeea8cf
Yuta Muraki <crowizard@gmail.com>
2019-02-09 16:04:13 +0900

    不要なファイルの削除

--- server/tools/install/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#yum -y install wget
-wget http://ftp.mozilla.org/pub/firefox/releases/45.0.2/linux-x86_64/ja/firefox-45.0.2.tar.bz2
-tar xvjf firefox-45.0.2.tar.bz2
-#mv firefox /opt/
-
-#ln -s /opt/firefox/firefox /usr/bin/firefox
-
