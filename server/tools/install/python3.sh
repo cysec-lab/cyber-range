4c797a973a6a80bdfa9134166b25b30513eda56c
Yuta Muraki <crowizard@gmail.com>
2019-02-09 16:28:37 +0900

    python3のバージョンを3.5 -> 3.6に変更

--- server/tools/install/python3.sh
+++ server/tools/install/python3.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 yum -y install https://centos6.iuscommunity.org/ius-release.rpm
-yum install -y python35*
+yum install -y python36*
 
-ln -s /usr/bin/python3.5 /usr/bin/python3
-ln -s /usr/bin/pip3.5 /usr/bin/pip3
+ln -s /usr/bin/python3.6 /usr/bin/python3
+ln -s /usr/bin/pip3.6 /usr/bin/pip3
