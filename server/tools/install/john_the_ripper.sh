4fab36bd87ed664c4815c18526380b5e47312794
Yuta Muraki <crowizard@gmail.com>
2019-02-09 17:03:13 +0900

    john the ripperインストールスクリプトのディレクトリ変更＋修正コメントの追加

--- server/tools/install/john_the_ripper.sh
+++ server/tools/install/john_the_ripper.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+#TODO: インストール出来ていない
 
 #rpm -import http://ftp.riken.jp/Linux/dag/RPM-GPG-KEY.dag.txt
 wget http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
 rpm -ivh rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
-yum install john
+yum -y install john # No package john available
