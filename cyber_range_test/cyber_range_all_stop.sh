006759bd94d9502d82479be1cfe8001b4bb70a7e
hedgehoCrow <crowizard@gmail.com>
2019-02-09 14:38:08 +0900

    サイバーレンジ環境すべてのVMをストップするスクリプトの追加

---
+++ cyber_range_test/cyber_range_all_stop.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# stop cyber_range environment
+# - select delete group number range
+#   if select 5 stop 1~5 group's vms
+
+tool_dir=/root/github/cyber_range/server/tools/proxmox # proxmox tool dir
+LOG_FILE="./setup.log"
+
+# Get JSON data
+json_vm_data=`cat vm_info.json`
+json_scenario_data=`cat scenario_info.json`
+day=`echo $json_scenario_data | jq '.day'`
+group_num=`echo $json_scenario_data | jq '.group_num'`
+student_per_group=`echo $json_scenario_data | jq '.student_per_group'`
+scenario_nums=`echo $json_scenario_data | jq ".days[$((day - 1))].scenario_nums[].scenario_num"`
+
+# TODO: Decide to WEB_NUMS and CLIENT_NUMS setting rules
+loop_num=1 # 1から始まる通し番号
+for _ in $scenario_nums; do
+    for g_num in `seq 1 $group_num`; do
+        VYOS_NUMS+=("${g_num}${loop_num}1") # vyos number is **1
+        WEB_NUMS+=("${g_num}${loop_num}2")  # web server number is **2
+        for i in `seq 3 $((2 + $student_per_group))`; do
+            CLIENT_NUMS+=("${g_num}${loop_num}${i}") # client pc number are **3 ~ **9
+        done
+    done
+    let "loop_num=loop_num+1" # increment
+done
+
+start_time=`date +%s`
+
+# delete before vms
+for num in ${VYOS_NUMS[@]} ${WEB_NUMS[@]} ${CLIENT_NUMS[@]}; do
+    qm stop $num # stop vm script
+done
+
+end_time=`date +%s`
+
+time=$((end_time - start_time))
+echo $time
+
+# output logs
+echo "[`date "+%Y/%m/%d %H:%M:%S"`] $0 $*" >> $LOG_FILE
+echo " time              : $time [s]" >> $LOG_FILE
+echo " group_num         : $group_num" >> $LOG_FILE
+echo " router_vms:       : ${VYOS_NUMS[@]}" >> $LOG_FILE
+echo " server_vms:       : ${WEB_NUMS[@]}" >> $LOG_FILE
+echo " client_vms:       : ${CLIENT_NUMS[@]}" >> $LOG_FILE
+echo >> $LOG_FILE
