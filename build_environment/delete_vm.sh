bbb8b3af7b91dfffc741245e532fe9d5283a4158
hedgehoCrow <crowizard@gmail.com>
2019-02-18 11:19:29 +0900

    deleteスクリプトをシンボリックリンクに変更

--- build_environment/delete_vm.sh
+++ build_environment/delete_vm.sh
@@ -1,15 +1,1 @@
-#!/bin/sh
-
-if [ $# -ne 1 ]; then
-    echo "delete vm num need"
-    echo "example:"
-    echo "$0 111"
-    exit 1
-fi
-
-file="/etc/pve/qemu-server/${1}.conf"
-if [ -e $file ]; then
-    qm stop $1
-    qm destroy $1
-    echo "$1 VM delete success"
-fi
+../server/tools/proxmox/delete_vm.sh
