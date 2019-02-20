e37f9ad4e5ac13e961ae79a7468a5cc243d86dbc
hedgehoCrow <crowizard@gmail.com>
2019-02-20 17:26:36 +0900

    john the ripperのインストールスクリプト修正

--- server/tools/install/centos6/john_the_ripper.sh
+++ server/tools/install/centos6/john_the_ripper.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
 #TODO: インストール出来ていない
+# wgetで落ちてきていない
+
+john_version='1.7.9-1' # ここのバージョンを変更するとインストールされるバージョンが変わる
+
+yum -y install wget
+result=`lscpu | grep 'Architecture' | grep 'x86_64'`
+if [ ${#result} -ne 0 ]; then
+    # 64bit
+    linux_version='x86_64'
+else
+    # 32bit
+    linux_version='i686'
+fi
+john_rpm="john-${john_version}.el6.rf.${linux_version}.rpm"
 
 #rpm -import http://ftp.riken.jp/Linux/dag/RPM-GPG-KEY.dag.txt
-wget http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
-rpm -ivh rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
+wget http://pkgs.repoforge.org/rpmforge-release/$john_rpm
+wget https://centos.pkgs.org/6/repoforge-${linux_version}/$john_rpm
+rpm -ivh $john_rpm
 yum -y install john # No package john available
