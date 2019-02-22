e31f8daa6ba3087c539e6340e7f80dd02dd11efa
hedgehoCrow <crowizard@gmail.com>
2019-02-22 16:25:32 +0900

    VMの設定ファイルとイメージをコピーするスクリプトを追加

---
+++ server/tools/utilities/copy_template_vm.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+# VMの設定ファイルとイメージをコピーするスクリプト
+
+if [ $# -ne 4 ]; then
+    echo "[ストレージ名(local | local-zfs)] [VM num] [コピー元ルートPATH] [コピー先ルートPATH]"
+    echo "$0 local-zfs 123 /mnt/hdd/pve/ /"
+    exit 1
+fi
+
+STRAGE_NAME=$1
+VM_NUM=$2
+SOURCE_DIR=$3
+DIST_DIR=$4
+
+QCOW2_DIR=var/lib/vz/images/
+CONF_PATH=etc/pve/qemu-server/
+
+convert_tool_dir=/root/github/cyber-range/server/tools/utilities/convert_image_format
+
+SOURCE_CONF_FILE="$SOURCE_DIR/$CONF_PATH/${VM_NUM}.conf"
+DIST_CONF_FILE="$DIST_DIR/$CONF_PATH/${VM_NUM}.conf"
+
+# ストレージ名チェック
+if !([ "$STRAGE_NAME" = 'local' ] || [ "$STRAGE_NAME" = 'local-zfs' ] || [ "$STRAGE_NAME" = 'local-tvm' ]); then
+    echo 'ストレージ名はlocal || local-zfs || local-tvmである必要があります'
+    exit 1
+fi
+
+# VM番号チェック
+result=`qm status $VM_NUM | grep 'does not exist'`
+if [ ${#result} -ne 0 ]; then
+    echo "番号 $VM_NUM のVMは存在していません"
+    exit 1
+fi
+
+# 設定ファイルチェック
+if [ -e "$DIST_CONF_FILE" ]; then
+    echo "すでに $DIST_CONF_FILE の設定ファイルが存在しています"
+    exit 1
+fi
+if [ ! -e "$SOURCE_CONF_FILE" ]; then
+    echo "コピー元 $SOURCE_CONF_FILE の設定ファイルが存在しません"
+    exit 1
+fi
+
+# 設定ファイルとイメージのコピー
+cp $SOURCE_CONF_FILE $DIST_CONF_FILE
+if [ "$STRAGE_NAME" = 'local' ]; then
+    # 設定ファイルの修正
+    result=`cat $DIST_CONF_FILE | grep -e "^ide0" | grep -e "local:$VM_NUM"`
+    if [ ${#result} -ne 0 ]; then
+        # local-zfs -> localに設定変更
+        sed -ie "s/-zfs:/:${VM_NUM}\//g" $DIST_CONF_FILE
+        sed -ie "s/disk-1/disk-1.qcow2/g" $DIST_CONF_FILE
+        sed -ie "/^scsihw:/d" $DIST_CONF_FILE
+    fi
+    # イメージのコピー
+    cp -rf $SOURCE_DIR/$QCOW2_DIR/$VM_NUM $DIST_DIR/$QCOW2_DIR/
+else
+    # 設定ファイルの修正
+    result=`cat $DIST_CONF_FILE | grep -e "^ide0" | grep -e "local-zfs"`
+    if [ ${#result} -ne 0 ]; then
+        # local -> local-zfsに設定変更
+        sed -ie "s/:$VM_NUM\//-zfs:/g" $DIST_CONF_FILE
+        sed -ie "s/disk-1.qcow2/disk-1/g" $DIST_CONF_FILE
+        sed -ie "/^ostype:/a scsihw: virtio-scsi-pci" $DIST_CONF_FILE
+    fi
+    # イメージのコピー
+    $convert_tool_dir/convert_qcow2_to_zfs.sh $VM_NUM rpool 32 $SOURCE_DIR/$QCOW2_DIR/$VM_NUM
+fi
