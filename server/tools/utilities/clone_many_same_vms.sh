8c3d57978cdc3157e5ed03c18b7f9a8342efd8d7
hedgehocrow <crowizard@gmail.com>
2019-02-24 12:52:26 +0900

    バグ修正

--- server/tools/utilities/clone_many_same_vms.sh
+++ server/tools/utilities/clone_many_same_vms.sh
@@ -14,11 +14,10 @@ START_NUM=$3
 END_NUM=$4
 
 # Get JSON data
-json_conf_data=`cat json_files/config_info.json`
-git_home_get_command=`echo $json_conf_data | jq '.git_home_get_command' | sed 's/"//g'`
-git_home=`$git_home_get_command`
+git_home=`git rev-parse --show-toplevel`
+json_conf_data=`cat $git_home//build_environment/json_files/config_info.json`
 TOOL_DIR=$git_home`echo $json_conf_data | jq '.tool_dir' | sed 's/"//g'`
-CONF_DIR=$git_home`echo $json_conf_data | jq '.vm_config_dir' | sed 's/"//g'`
+CONF_DIR=`echo $json_conf_data | jq '.vm_config_dir' | sed 's/"//g'`
 
 
 # CLONE_TYPEのチェック
@@ -51,10 +50,10 @@ done
 # クローン後のVM名は「テンプレートVMの名前 + 数字」とするので、テンプレートVMの名前を取得する
 TEMPLATE_VM_NAME=`grep -e "^name:" $CONF_DIR/${TEMPLATE_VM_NUM}.conf | awk '{print $2}'`
 # VMのブリッジ番号はテンプレートVMと同じものを利用する
-TEMPLATE_VM_BRIDGE_NUMS=(`grep -e "^net" /etc/pve/qemu-server/${TEMPLATE_VM_NUM}.conf | awk -F 'vmbr' '{print $2}')
+TEMPLATE_VM_BRIDGE_NUMS=(`grep -e "^net" /etc/pve/qemu-server/${TEMPLATE_VM_NUM}.conf | awk -F 'vmbr' '{print $2}'`)
 
 # VMのクローン
-for ((new_num=$START_NUM, i=1; new_vm_num <= $END_NUM; new_vm_num++, i++)); do
+for ((new_vm_num=$START_NUM, i=1; new_vm_num <= $END_NUM; new_vm_num++, i++)); do
     NEW_VM_NAME=$TEMPLATE_VM_NAME$i
     bash $TOOL_DIR/clone_vm.sh $CLONE_TYPE $new_vm_num $TEMPLATE_VM_NUM  $TEMPLATE_VM_NAME local-zfs ${TEMPLATE_VM_BRIDGE_NUMS[@]}
 done
