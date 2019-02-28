87b47e7030915db90bd52e74c25829b89421f74b
Yuta Muraki <crowizard@gmail.com>
2019-02-28 16:59:44 +0900

    MACアドレスを大文字にするとVyOSのインターフェースが識別しなくなるバグ修正対応漏れ

--- server/tools/utilities/copy_template_vm.sh
+++ server/tools/utilities/copy_template_vm.sh
@@ -69,7 +69,7 @@ BRIDGE_NUMS=(`grep -e "^net" $DIST_CONF_FILE | awk -F 'vmbr' '{print $2}'`)
 # コピーしたVMのMACアドレスを変更
 for ((i=0;i<${#BRIDGE_NUMS[@]};i++)); do
     # 新たなMACアドレスを生成
-    mac_address=`dd if=/dev/urandom bs=1024 count=1 2>/dev/null|md5sum|sed 's/^\(..\)\(..\)\(..\)\(..\)\(..\).*$/02:\1:\2:\3:\4:\5/' | tr '[a-z]' '[A-Z]'`
+    mac_address=`dd if=/dev/urandom bs=1024 count=1 2>/dev/null|md5sum|sed 's/^\(..\)\(..\)\(..\)\(..\)\(..\).*$/02:\1:\2:\3:\4:\5/'`
     new_net_info="net${i}: e1000=${mac_address},bridge=vmbr${BRIDGE_NUMS[i]}"
     # clone時にconfファイルにparentとして以前のデータが残ることがあるが前のデータを残すために最初にマッチした行のみ変更を加える
     sed -i -e "0,/^net${i}/s/^net${i}.*/$new_net_info/g" $DIST_CONF_FILE
