324bed723545e1a817eba7798380fe8cbced476f
hedgehocrow <crowizard@gmail.com>
2019-03-04 21:24:20 +0900

    コメントの追加+不要な処理の削除

--- server/tools/proxmox/clone.sh
+++ server/tools/proxmox/clone.sh
@@ -19,30 +19,15 @@ NETWORKFILE="${MOUNT_DIR}/etc/sysconfig/network"
 CRONFILE="${MOUNT_DIR}/var/spool/cron/root"
 CONF_PATH="/etc/pve/qemu-server/${VM_NUM}.conf"
 
-#while read line
-#do
-#  case "$line" in
-#    *0x8086* ) doc=${line}"\n" ;;
-#    *eth0* ) conf=${line} ;;
-#    #*eth1* ) conf=${line} ;;
-#  esac
-#done < ${RULEFILE}
-
+# VMクローン時にProxmoxが新たに生成したMACアドレスをVM内の設定に適応する(新MACアドレスはProxmoxの設定ファイルのみ変更されている．VM内の設定はクローン元と同じ)
 NET0=`cat $CONF_PATH | grep -m 1 "net0"`
 e1000=${NET0%,*}
 HWADDR=${e1000#*e1000=}
 sed -i -e "s/ATTR{address}==\".*\",/ATTR{address}==\"${HWADDR,,}\",/g" $RULEFILE
-
-# 改行文字の問題解決
-#doc=${doc}${conf}
-#echo ${doc%\\n} | sed -e 's/\\n/\n/g' -e 's/eth1/eth0/g' > ${RULEFILE}
 sed -i -e 's/eth1/eth0/g' $RULEFILE
 
-
-#attr=${conf#*ATTR{address\}==\"}
-#sed -i -e "s/HWADDR=.*$/HWADDR=${attr%%\"*}/g" $ETHFILE
+# IPアドレスなどサイバーレンジに利用する設定に変更する
 sed -i -e "s/HWADDR=.*$/HWADDR=${HWADDR}/g" $ETHFILE
-
 sed -i -e "s/UUID.*//g" $ETHFILE
 sed -i -e "s/IPADDR=.*$/IPADDR=$IP_ADDRESS/g" $ETHFILE
 sed -i -e "s/DNS1=.*$/DNS1=${IP_ADDRESS%.*}.1/g" $ETHFILE
@@ -51,6 +36,3 @@ sed -i -e "s/BROADCAST=.*$/BROADCAST=${IP_ADDRESS%.*}.255/g" $ETHFILE
 
 # change hostname
 sed -i -e "s/HOSTNAME=.*/HOSTNAME=${HOSTNAME}/g" $NETWORKFILE
-
-# crontab comment delete
-#sed -i -e "s/^#//g" $CRONFILE
